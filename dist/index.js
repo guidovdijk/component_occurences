2f57138d600a7a9ad8b43d90917fed2c2bc68436
guidovdijk <funnydutchguy@gmail.com>
2023-05-01 15:04:41 +0200

    added warning

--- dist/index.js
+++ dist/index.js
@@ -9870,11 +9870,10 @@ const run = async() => {
 
     const NOT_USED_PACKAGES = await getAllOccurrences(componentNames, allFiles, COMPONENT_OCCURRENCE_REGEX)
 
-    core.info(`NOT_USED_COMPONENTS: ${JSON.stringify(NOT_USED_PACKAGES)}`);
     core.setOutput("NOT_USED_COMPONENTS", JSON.stringify(NOT_USED_PACKAGES));
-
+    
     if(NOT_USED_PACKAGES && NOT_USED_PACKAGES.length > 0){
-      throw new Error("Unused components are found")
+      core.warning("Unused components are found")
     }
   } catch (error) {
     core.setFailed(error.message);
