fc9c2df4b477f87151d51159b2000b056cecabbf
guidovdijk <funnydutchguy@gmail.com>
2023-05-01 12:29:36 +0200

    check if unused components get logged

--- test_folder/screen.tsx
+++ test_folder/screen.tsx
@@ -1,6 +1,6 @@
 import Test from './components/Test'
 
-export default function TimelineScreen({ navigation }: Props) {
+export default function TimelineScreen() {
   return (
     <Test></Test>
   )
